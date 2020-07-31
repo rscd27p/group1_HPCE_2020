fd3f6996139e84e1808f7fcd5a9d46cfbaaeda42
Andres <agomez10010@gmail.com>
2020-07-30 21:01:00 -0600

    Adding final code for intrinsics optimization and output YUV file

--- project_2/src/rgb2yuv_intrinsics/main.cpp
+++ project_2/src/rgb2yuv_intrinsics/main.cpp
@@ -151,101 +151,135 @@ size_t load_input_file(char * file_name, unsigned char ** input_image){
 
 // Function to convert RGB to YUV
 void rgb2yuv(unsigned char * __restrict input_image, unsigned char * __restrict output_image, uint32_t total_bytes){
-	// Coefficients for Y calculation. Each register is 64 bytes (8 repeated coefficients, each 1 byte)
-	uint8x8_t y_r_coeff = vdup_n_u8(66);
-	uint8x8_t y_g_coeff = vdup_n_u8(129);
-	uint8x8_t y_b_coeff = vdup_n_u8(25);
-
-	// Coefficients for U calculation. Each register is 128 bytes (8 repeated coefficients, each 2 bytes)
-	int16x8_t u_r_coeff = vdupq_n_s16(-38);
-	int16x8_t u_g_coeff = vdupq_n_s16(-74);
-	int16x8_t u_b_coeff = vdupq_n_s16(112);
-
-	// Coefficients for V calculation. Each register is 128 bytes (8 repeated coefficients, each 2 bytes)
-	int16x8_t v_r_coeff = vdupq_n_s16(112);
-	int16x8_t v_g_coeff = vdupq_n_s16(-94);
-	int16x8_t v_b_coeff = vdupq_n_s16(-18);
-
-
 	// Temporal registers for calculations. Each of them has 8, 16-bit registers
-	uint16x8_t  Y_tmp;
-	int16x8_t  U_tmp;
-	int16x8_t  V_tmp;
+	uint16x8_t  tmp_high_u;
+	uint16x8_t  tmp_low_u;
+	int16x8_t  tmp_high_s;
+	int16x8_t  tmp_low_s;
 
+	// Offset constant
+	const int16x8_t  offset_128_i = vdupq_n_s16(128);
 
-	// Offset constants
-	uint16x8_t  offset_128_u = vdupq_n_u16(128);
-	uint16x8_t  offset_16_u = vdupq_n_u16(16);
-	int16x8_t  offset_128_i = vdupq_n_s16(128);
-	int16x8_t  offset_16_i = vdupq_n_s16(16);
-
-	uint32_t iterations = total_bytes / 24;
+	// In each iteration of the for loop below we will process 16 pixels (48 bytes)
+	uint32_t iterations = total_bytes / 48;
 	uint32_t it_count = 0;
 	uint32_t bytes_count = 0;
 
-	// Triple register to store yuv results
-	uint8x8x3_t yuv;
+	// Triple register to store yuv results. Each register has 16, 8-bits registers
+	uint8x16x3_t yuv;
 
 	// Run through calculated number of iterations
 	for(it_count=0; it_count < iterations; it_count++){
-		// Extract R, G and B. We are loading in one call 8 bytes into 3 separate registers
-		uint8x8x3_t rgb  = vld3_u8(input_image + bytes_count);
+		// Extract R, G and B onto triple register. Each register has 16, 8-bits registers
+		uint8x16x3_t rgb  = vld3q_u8(input_image + bytes_count);
+
+		// We are going to process each color in two chunks.
+		uint8x8_t R_high_u = vget_high_u8(rgb.val[0]);
+		uint8x8_t R_low_u = vget_low_u8(rgb.val[0]);
+		uint8x8_t G_high_u = vget_high_u8(rgb.val[1]);
+		uint8x8_t G_low_u = vget_low_u8(rgb.val[1]);
+		uint8x8_t B_high_u = vget_high_u8(rgb.val[2]);
+		uint8x8_t B_low_u = vget_low_u8(rgb.val[2]);
 
 		// ******  Calculating Y ********
 		//Y_tmp = ((66*R + 129*G + 25*B) + 128) >> 8;
 		//output_image[bytes_counter] = (uint8_t) CLIP_uint16(Y_tmp + 16);
-		// Multiply rgb by coefficients to obtain Y_tmp. Multiply values 8 by 8 and accumulate result on Y_tmp
-		Y_tmp = vmull_u8(rgb.val[0], y_r_coeff);
-		Y_tmp = vmlal_u8(Y_tmp, rgb.val[1], y_g_coeff);
-		Y_tmp = vmlal_u8(Y_tmp, rgb.val[2], y_b_coeff);
-		// Add 128 constant to the 8, 16-bit registers
-		Y_tmp = vaddq_u16(Y_tmp, offset_128_u);
-		// Now shift 8 bits to the right
-		Y_tmp = vshrq_n_u16(Y_tmp, 8);
-		// Add 16 constant to the 8, 16-bit registers
-		Y_tmp = vaddq_u16(Y_tmp, offset_16_u);
-		// Now saturate and convert back to 8, 8-bits registers which is final result for Y
-		yuv.val[0] = vqmovn_u16(Y_tmp);
+		// Perform multiplications by coefficients and accumulate for each of the 2 chunks
+		tmp_high_u = vmull_u8(R_high_u, vdup_n_u8(66));
+		tmp_low_u = vmull_u8(R_low_u, vdup_n_u8(66));
 
+		tmp_high_u = vmlal_u8(tmp_high_u, G_high_u, vdup_n_u8(129));
+		tmp_low_u = vmlal_u8(tmp_low_u, G_low_u, vdup_n_u8(129));
+
+		tmp_high_u = vmlal_u8(tmp_high_u, B_high_u, vdup_n_u8(25));
+		tmp_low_u = vmlal_u8(tmp_low_u, B_low_u, vdup_n_u8(25));
+
+		// Add 128 constant
+		tmp_high_u = vaddq_u16(tmp_high_u, vdupq_n_u16(128));
+		tmp_low_u = vaddq_u16(tmp_low_u, vdupq_n_u16(128));
+
+		// Now shift 8 bits to the right
+		tmp_high_u = vshrq_n_u16(tmp_high_u, 8);
+		tmp_low_u = vshrq_n_u16(tmp_low_u, 8);
+
+		// Add 16 constant
+		tmp_high_u = vaddq_u16(tmp_high_u, vdupq_n_u16(16));
+		tmp_low_u = vaddq_u16(tmp_low_u, vdupq_n_u16(16));
+
+		// Now saturate and convert back to 16, 8-bits registers which is final result for Y
+		yuv.val[0] = vcombine_u8(vqmovn_u16(tmp_low_u), vqmovn_u16(tmp_high_u));
+
+		// For U and V we need new values for RGB that are signed. In the same way, process in 2 chunks.
+		int16x8_t R_high_s = vreinterpretq_s16_u16(vaddl_u8(R_high_u, vdup_n_u8(0)));
+		int16x8_t R_low_s = vreinterpretq_s16_u16(vaddl_u8(R_low_u, vdup_n_u8(0)));
+		int16x8_t G_high_s = vreinterpretq_s16_u16(vaddl_u8(G_high_u, vdup_n_u8(0)));
+		int16x8_t G_low_s = vreinterpretq_s16_u16(vaddl_u8(G_low_u, vdup_n_u8(0)));
+		int16x8_t B_high_s = vreinterpretq_s16_u16(vaddl_u8(B_high_u, vdup_n_u8(0)));
+		int16x8_t B_low_s = vreinterpretq_s16_u16(vaddl_u8(B_low_u, vdup_n_u8(0)));
 
 		// ******  Calculating U ********
 		//U_tmp = ((-38*R - 74*G + 112*B) + 128) >> 8;
 		//output_image[bytes_counter + 1] = (uint8_t) CLIP_int16(U_tmp + 128);
-		// Multiply rgb by coefficients to obtain U_tmp. Multiply values 8 by 8 and accumulate result on U_tmp
-		U_tmp = vmulq_s16(vreinterpretq_s16_u16(vmovl_u8 (rgb.val[0])), u_r_coeff);
-		U_tmp = vmlaq_s16(U_tmp, vreinterpretq_s16_u16(vmovl_u8 (rgb.val[1])), u_g_coeff);
-		U_tmp = vmlaq_s16(U_tmp, vreinterpretq_s16_u16(vmovl_u8 (rgb.val[2])), u_b_coeff);
-		// Add 128 constant to the 8, 16-bit registers
-		U_tmp = vaddq_s16(U_tmp, offset_128_i);
+		// Perform multiplications by coefficients and accumulate for each of the 2 chunks
+		tmp_high_s = vmulq_s16(R_high_s, vdupq_n_s16(-38));
+		tmp_low_s = vmulq_s16(R_low_s, vdupq_n_s16(-38));
+
+		tmp_high_s = vmlaq_s16(tmp_high_s, G_high_s, vdupq_n_s16(-74));
+		tmp_low_s = vmlaq_s16(tmp_low_s, G_low_s, vdupq_n_s16(-74));
+
+		tmp_high_s = vmlaq_s16(tmp_high_s, B_high_s, vdupq_n_s16(112));
+		tmp_low_s = vmlaq_s16(tmp_low_s, B_low_s, vdupq_n_s16(112));
+
+		// Add 128 constant
+		tmp_high_s = vaddq_s16(tmp_high_s, offset_128_i);
+		tmp_low_s = vaddq_s16(tmp_low_s, offset_128_i);
+
 		// Now shift 8 bits to the right
-		U_tmp = vshrq_n_s16(U_tmp, 8);
-		// Add 128 constant to the 8, 16-bit registers
-		U_tmp = vaddq_s16(U_tmp, offset_128_i);
-		// Now saturate and convert back to 8, 8-bits registers which is final result for U
-		yuv.val[1] = vqmovun_s16(U_tmp);
+		tmp_high_s = vshrq_n_s16(tmp_high_s, 8);
+		tmp_low_s = vshrq_n_s16(tmp_low_s, 8);
+
+		// Add 128 constant
+		tmp_high_s = vaddq_s16(tmp_high_s, offset_128_i);
+		tmp_low_s = vaddq_s16(tmp_low_s, offset_128_i);
+
+		// Now saturate and convert back to 16, 8-bits registers which is final result for U
+		yuv.val[1] = vcombine_u8(vqmovun_s16(tmp_low_s), vqmovun_s16(tmp_high_s));
+
 
 		// ******  Calculating V ********
 		//V_tmp = ((112*R - 94*G - 18*B) + 128) >> 8;
 		//output_image[bytes_counter + 2] = (uint8_t) CLIP_int16(V_tmp + 128);
-		// Multiply rgb by coefficients to obtain U_tmp. Multiply values 8 by 8 and accumulate result on U_tmp
-		V_tmp = vmulq_s16(vreinterpretq_s16_u16(vmovl_u8 (rgb.val[0])), v_r_coeff);
-		V_tmp = vmlaq_s16(V_tmp, vreinterpretq_s16_u16(vmovl_u8 (rgb.val[1])), v_g_coeff);
-		V_tmp = vmlaq_s16(V_tmp, vreinterpretq_s16_u16(vmovl_u8 (rgb.val[2])), v_b_coeff);
-		// Add 128 constant to the 8, 16-bit registers
-		V_tmp = vaddq_s16(V_tmp, offset_128_i);
+		// Perform multiplications by coefficients and accumulate for each of the 2 chunks
+		tmp_high_s = vmulq_s16(R_high_s, vdupq_n_s16(112));
+		tmp_low_s = vmulq_s16(R_low_s, vdupq_n_s16(112));
+
+		tmp_high_s = vmlaq_s16(tmp_high_s, G_high_s, vdupq_n_s16(-94));
+		tmp_low_s = vmlaq_s16(tmp_low_s, G_low_s, vdupq_n_s16(-94));
+
+		tmp_high_s = vmlaq_s16(tmp_high_s, B_high_s, vdupq_n_s16(-18));
+		tmp_low_s = vmlaq_s16(tmp_low_s, B_low_s, vdupq_n_s16(-18));
+
+		// Add 128 constant
+		tmp_high_s = vaddq_s16(tmp_high_s, offset_128_i);
+		tmp_low_s = vaddq_s16(tmp_low_s, offset_128_i);
+
 		// Now shift 8 bits to the right
-		V_tmp = vshrq_n_s16(V_tmp, 8);
-		// Add 128 constant to the 8, 16-bit registers
-		V_tmp = vaddq_s16(V_tmp, offset_128_i);
-		// Now saturate and convert back to 8, 8-bits registers which is final result for U
-		yuv.val[2] = vqmovun_s16(V_tmp);
+		tmp_high_s = vshrq_n_s16(tmp_high_s, 8);
+		tmp_low_s = vshrq_n_s16(tmp_low_s, 8);
 
+		// Add 128 constant
+		tmp_high_s = vaddq_s16(tmp_high_s, offset_128_i);
+		tmp_low_s = vaddq_s16(tmp_low_s, offset_128_i);
 
-		// Store results interleaved to output image
-		vst3_u8(output_image + bytes_count, yuv);
+		// Now saturate and convert back to 16, 8-bits registers which is final result for V
+		yuv.val[2] = vcombine_u8(vqmovun_s16(tmp_low_s), vqmovun_s16(tmp_high_s));
+
+		// Store yuv results interleaved to output image
+		vst3q_u8(output_image + bytes_count, yuv);
 
 		// Increase bytes counter with progress
-		bytes_count += 24;
+		bytes_count += 48;
 	}
 }
+
 
