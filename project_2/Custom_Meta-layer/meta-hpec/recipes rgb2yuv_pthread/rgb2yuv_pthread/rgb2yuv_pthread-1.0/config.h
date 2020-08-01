e1bc69ae79c22e885b279491f6c674695640a8d7
Randy Cespedes <rcespedes27dds@gmail.com>
2020-07-31 23:40:59 -0600

    adding recipes for four programs

---
+++ project_2/Custom_Meta-layer/meta-hpec/recipes rgb2yuv_pthread/rgb2yuv_pthread/rgb2yuv_pthread-1.0/config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "rgb2yuv_pthread"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "sercr0388@address"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "rgb2yuv_pthread"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "rgb2yuv_pthread 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "rgb2yuv_pthread"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* Version number of package */
+#define VERSION "1.0"
